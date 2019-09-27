b4892d1e51f9d85023aa1918722ffd47276b390f
dharamsh <draj.sharma7654+@gmail.com>
2019-09-28 00:45:02 +0530

    Initial commit 1.0

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("This is HelloWorld Application!! "+getName());
+
+    }
+    public static String getName(){
+        return "Dharam";
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+public class HelloWorldTest extends HelloWorld {
+
+    @org.junit.Test
+    public void testGetName() {
+    }
+}
